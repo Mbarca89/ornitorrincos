3a0f42f6b7490a6668d43dc8d9e53d07e43fe1da
ErnestoAli <aliernesto05@gmail.com>
2025-08-22 20:25:04 -0300

    Update OrnitorrincoVerde.java
    ornitorrincoVerde

--- src/ornitorrinco/OrnitorrincoVerde.java
+++ src/ornitorrinco/OrnitorrincoVerde.java
@@ -8,6 +8,29 @@ package ornitorrinco;
  *
  * @author Nicolino Uchiha
  */
-public class OrnitorrincoVerde {
-    
+public class OrnitorrincoVerde extends Castor implements MamaPata {
+    private String nombre;
+    private double pico = 6.8;
+    private double patas = 7.5;
+
+    public OrnitorrincoVerde(String nombre, double longiCola, double velocidad) {
+        super(longiCola, velocidad);
+        this.nombre = nombre;
+    }
+
+    public void tocarOrgano() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("ingrese una tecla para empezar el órgano");
+        sc.nextLine();
+        System.out.println("Do-Re-Mi");
+        System.out.println("Fa-Sol-La-Si");
+    }
+
+    public void tocarGuitorgan() {
+        System.out.println(nombre + " toca el Guitorgan:");
+        super.tocarGuitarra();
+        this.tocarOrgano();
+        System.out.println("cuac cuac…!");
+    }
+
 }
