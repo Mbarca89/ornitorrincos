1f0a2fe35e774b726c4f1c380d7593d09c31d1c1
Mbarca89 <mauriciobarca1989@gmail.com>
2025-08-22 14:06:48 -0300

    ornitorrinco azul

--- src/ornitorrinco/OrnitorrincoAzul.java
+++ src/ornitorrinco/OrnitorrincoAzul.java
@@ -9,8 +9,32 @@ package ornitorrinco;
  *
  * @author Mauricio
  */
-public class OrnitorrincoAzul extends Castos implements Pato {
+public class OrnitorrincoAzul extends Castor implements Pato {
+    
+    private int propulsion;
+
+    public OrnitorrincoAzul(int propulsion, int velocidadNadar, double anchoCola) {
+        super(velocidadNadar, anchoCola);
+        this.propulsion = propulsion;
+    }
+
     
     
+    public int getPropulsion() {
+        return propulsion;
+    }
+
+    public void setPropulsion(int propulsion) {
+        this.propulsion = propulsion;
+    }
     
+    @Override
+    public void nadar () {
+        System.out.println("Estoy nadando con propulsion a " + super.getVelocidadNadar() + propulsion + " Km/H");
+    }
+    
+    @Override
+    public void tocarOrgano(){
+        return;
+    }   
 }
