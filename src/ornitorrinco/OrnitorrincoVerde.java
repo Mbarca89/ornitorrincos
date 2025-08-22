e4ff5fc4f6312eb981bdef5cb06b44974e194f93
ErnestoAli <aliernesto05@gmail.com>
2025-08-22 20:27:45 -0300

    Update OrnitorrincoVerde.java

--- src/ornitorrinco/OrnitorrincoVerde.java
+++ src/ornitorrinco/OrnitorrincoVerde.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package ornitorrinco;
-
+import java.util.Scanner;
 /**
  *
  * @author Nicolino Uchiha
