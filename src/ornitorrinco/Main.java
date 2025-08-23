4f851ddd6bf4bf630ecce5943f8eb57174089a0a
joaquinrodriguez19 <jjjoaquinrodriguez@gmail.com>
2025-08-22 23:08:42 -0300

    main orniverde

--- src/ornitorrinco/Main.java
+++ src/ornitorrinco/Main.java
@@ -5,6 +5,9 @@
  */
 package ornitorrinco;
 
+import java.time.LocalDate;
+import java.util.Scanner;
+
 /**
  *
  * @author Mauricio
@@ -16,7 +19,28 @@ public class Main {
      */
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
         
+        LocalDate today = LocalDate.now();
+        System.out.println("Fecha de ejecución: " + today);
+        
+        System.out.println("Ingresa el nombre del OrnitorrincoVerde:");
+        
+        String nombreOrnitorrincoVerde = scanner.nextLine();
+        
+        System.out.println("Ingresa la longitud de cola del OrnitorrincoVerde:");
+        
+        double colaVerde = scanner.nextDouble();
+        
+        System.out.println("Ingresa la velocidad de nado del padre (para el OrnitorrincoVerde):");
+        
+        double velVerde = scanner.nextDouble();
+        
+        
+        
+        
+                
+
     }
     
 }
